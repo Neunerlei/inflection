0efbcd6242304762ab1840b2841d8a0c8392b9d4
Martin Neundorfer <m.neundorfer@labor.digital>
2020-03-11 18:36:47 +0100

    feat: remove dependency on neunerlei/options that was actually not required at all

--- Classes/Inflector.php
+++ Classes/Inflector.php
@@ -21,7 +21,6 @@ declare(strict_types=1);
 
 namespace Neunerlei\Inflection;
 
-
 use Neunerlei\Inflection\Adapter\InflectorAdapterInterface;
 use Neunerlei\Inflection\Adapter\SymfonyInflectorAdapter;
 
